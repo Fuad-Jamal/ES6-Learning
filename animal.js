46a982846d25b70fc9e0808e9b70061b24c7eb7e
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-04 10:44:17 +0200

    creating animal checker program

---
+++ animal.js
@@ -0,0 +1,12 @@
+console.log('Animal checker')
+let animal = 'dogr'
+if(animal=== 'cat'){
+    console.log('animal is cat')
+}
+else if(animal === 'dog'){
+    console.log('animal is dog')
+}
+else if(animal !== 'cat'|| animal !== 'dog'){
+    console.log('animal is not cat or dog')
+}
+console.log('Animal check complete')

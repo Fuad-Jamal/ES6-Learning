71ff922469cc92fcbd00946d2f2dbec76ccd9d31
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-06 21:07:47 +0200

    breaking and continuing in loops

--- continue.js
+++ continue.js
@@ -1,7 +1,11 @@
-for(let i = 0; i<100; i++){
+for(let i = 0; i<100; i+=10){
     console.log(i)
     if(i === 20){
         i += 20
     continue;
     }
+    console.log('loop num', i)
+if(i==60){
+    break;
+}
 }

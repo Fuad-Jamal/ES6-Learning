47b985844660111e6dac38de13ccf6c43f3ef366
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-07 07:38:56 +0200

    continue exercise in loops

--- continue.js
+++ continue.js
@@ -17,3 +17,17 @@ for(let log= false, i=1; !log; i++){
     }
 }
 console.log('complete log')
+
+for(let i= 0; i<=100; i++){
+if( i === 50){
+    console.log('hagati')
+    continue;
+}else if (i === 100){
+    console.log('mwisho')
+    continue;
+}
+    if(i % 10 === 0){
+        console.log(`Counting: ${i}`)
+    }
+}
+console.log('twatoboye')

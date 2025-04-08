ba14d04e02e7a33f42567903479b2b277dda7c15
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-08 15:15:31 +0200

    array multiplication table

---
+++ array.js
@@ -0,0 +1,8 @@
+const mut = 80543
+let arr = [11,12,13,421,42,456,66,373]
+
+for(let i=0; i<arr.length; i++){
+    let iti = mut * arr[i]
+    console.log(`${arr[i]} * ${mut} = ${iti}`)
+}
+// console.log(it)

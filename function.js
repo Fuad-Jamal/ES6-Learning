34473eab7b618493f84b2576fe7fd65a5358c243
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-08 14:01:07 +0200

    logging function reveal

--- function.js
+++ function.js
@@ -1,8 +1,20 @@
-function chil(){
-    console.log('chili chili')
-    for( let i =1; i<=10; i++){
-        console.log(`chile at ${i}`)
-    }
-    console.log('all chili donex')
+// function chil(){
+//     console.log('chili chili')
+//     for( let i =1; i<=10; i++){
+//         console.log(`chile at ${i}`)
+//     }
+//     console.log('all chili donex')
+// }
+// chil()
+
+function log(use, pas){
+if(use === 'me') {return true}
+if(use === 'monn' && pas === 1212) {return true}
+if(use == 'tit' && pas === '444') {return true}
+
+return false
 }
-chil()
+console.log(log('mn',88))
+console.log(log('me', 844));
+console.log(log('monn',1212));
+console.log(log('tit', '444'))

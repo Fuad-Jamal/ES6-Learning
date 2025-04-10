350bf2d7029c905aca7bd210345d0eed68ea0275
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-10 16:30:18 +0200

    using math.max to find highest in array

--- object.js
+++ object.js
@@ -14,3 +14,43 @@ console.log(myPet)
 console.log(myPet)
 myPet.favoriteActivities.push("playing with toys")
 console.log(myPet)
+
+
+const apple = {
+    name: "Apple",
+    color: "Red",
+    taste: "Sweet"
+};
+
+const banana = {
+    name: "Banana",
+    color: "Yellow",
+    taste: "Sweet"
+};
+
+const orange = {
+    name: "Orange",
+    color: "Orange",
+    taste: "Citrus"
+};
+
+const product = {
+    id: 101,
+    category: "Fruits",
+    price: [15.99,44,43,324,142,2,62,14,52,52,4,25,234,52],
+    inStock: true,
+    fruits: [ apple, banana, orange ]
+};
+
+console.log(product.fruits[0]);
+
+for(let i = 0; i < product.fruits.length; i++){
+    console.log(product.fruits[i])
+}
+product.fruits[2].color = "Green"
+console.log(product.fruits[2])
+
+let x = product.price.reduce((acc, curr) => {
+
+},Math.max(...product.price))
+console.log(x)

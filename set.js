fe4beb1e06510249d35dd89a6e42d54c491b0a71
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-11 12:44:13 +0200

    pushing duplicates out using set

--- set.js
+++ set.js
@@ -12,3 +12,35 @@ for(let i=0; i<numbers.length; i++){
 yup.add(numbers[i])
 }
 console.log(yup)
+for(const ll of yup){
+    console.log(ll)
+}
+
+let seen = new Set()
+for(let i=0; i<numbers.length; i++){
+    if(seen.has(numbers[i])){
+        console.log(numbers[i])
+    break;
+    }
+seen.add(numbers[i])
+}
+console.log(seen) 
+
+let ay = [
+    { name: "Alice", age: 25 },
+    { name: "Alice", age: 259 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+];
+
+let result = []
+let duplicate = new Set()
+for(let i=0; i<ay.length; i++){
+    if(duplicate.has(ay[i].name)){
+        continue;
+    }
+    result.push(ay[i])
+    duplicate.add(ay[i].name)
+}
+console.log(result)
+console.log(duplicate)

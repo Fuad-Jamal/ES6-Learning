47ff8b9d48c63ab9778e6ec6eede1ca61557847e
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-11 15:55:14 +0200

    adding string methods to sets

--- for-of.js
+++ for-of.js
@@ -23,3 +23,46 @@ mySet.add(['array', 'example']);
 for (const item of mySet) {
     console.log(item);
 }
+
+// Creating a regular object with properties
+const myObject = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 25,
+    isEmployed: true,
+    skills: ['JavaScript', 'React', 'Node.js'],
+    address: {
+        city: 'Los Angeles',
+        zip: '90001'
+    }
+};
+
+for(const lee of Object.entries(myObject)){
+    console.log(lee)
+}
+
+const ages = [30, 25, 40, 18, 60, 22, 35, 50, 28, 45];
+const result = [];
+for(const age of ages){
+    const did = {
+        'age': age,
+        'name': 'John Doe'
+    }
+    result.push(did)
+}
+console.log(result)
+
+const namesSet = new Set();
+let tont = new Set()
+
+namesSet.add('Alice');
+namesSet.add('Bob');
+namesSet.add('Charlie');
+namesSet.add('Diana');
+namesSet.add('Eve');
+
+for(const name of namesSet) {
+    tont.add(name.toUpperCase())
+}
+console.log(tont)
+console.log(namesSet)

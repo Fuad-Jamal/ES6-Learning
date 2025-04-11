e08cb957c77ff8a2c4267c9b675d94ceb7ab75ff
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-11 11:35:09 +0200

    remove duplicates with set

--- set.js
+++ set.js
@@ -4,4 +4,11 @@ set.add('lkd sdj')
 set.add(true)
 set.add('osidih')
 set.delete('osidih')
-console.log(set)
+console.log(set.size)
+
+let yup = new Set()
+let numbers = [1, 2, 3, 4, 5, 3, 2, 6, 7, 8, 1];
+for(let i=0; i<numbers.length; i++){
+yup.add(numbers[i])
+}
+console.log(yup)

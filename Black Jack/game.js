559d5f87c6c29019fec2a84a940ad7cfef8e987a
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-13 09:13:58 +0200

    pushing 52 cards with corresponding suits to deck array

--- Black Jack/game.js
+++ Black Jack/game.js
@@ -1,3 +1,10 @@
 const deck = []
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 const cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+for(const card of cards){
+    for(const suit of suits){
+    deck.push({Card: card, Suit: suit})
+
+    }}
+console.log(deck)

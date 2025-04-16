12c85b0f2d61ac453c812085ec34cb0f632496e2
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-16 16:43:38 +0200

    using then when promise is resolved

--- promise.js
+++ promise.js
@@ -1,6 +1,11 @@
 const waka = new Promise((res,rej)=>{
     setTimeout(() => {
         console.log('Waka Waka!');
-    }, 1000);
+        res('don kd')
+    }, 2000);
 })
 // console.log(waka)
+waka.then((res)=>{
+    console.log('promise olevoir')
+    console.log(res)
+})

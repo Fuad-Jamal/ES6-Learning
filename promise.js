53f31347d34991e2aadd30e8de9a360837199e5b
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-17 12:09:43 +0200

    promise exercises

--- promise.js
+++ promise.js
@@ -1,20 +1,31 @@
-const waka = new Promise((res, rej) => {
-  setTimeout(() => {
-    console.log("Waka Waka!");
-    rej("don kd");
-  }, 2000);
-});
-// console.log(waka)
-waka.then((res) => {
-    console.log("promise olevoir");
-    console.log(res);
-    console.log(waka)
-    return "impala";
-  })
-  .then((chr) => {
-    console.log(chr);
-  })
-  .catch((error) => {
-    console.log(error);
-    console.log(waka)
-  });
+// const waka = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     console.log("Waka Waka!");
+//     rej("don kd");
+//   }, 2000);
+// });
+// // console.log(waka)
+// waka.then((res) => {
+//     console.log("promise olevoir");
+//     console.log(res);
+//     console.log(waka)
+//     return "impala";
+//   })
+//   .then((chr) => {
+//     console.log(chr);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     console.log(waka)
+//   });
+
+console.log("program start")
+let saka = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+console.log('program in progress')
+resolve('promise resolved')
+    },3000)
+}).then((res)=>{
+    console.log(res)
+})
+console.log(saka)

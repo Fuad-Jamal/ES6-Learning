f414ab8c7fc8c15aa1e95363c80acb9cfca33ce7
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-17 10:19:47 +0200

    rejecting promises

--- promise.js
+++ promise.js
@@ -1,11 +1,13 @@
 const waka = new Promise((res,rej)=>{
     setTimeout(() => {
         console.log('Waka Waka!');
-        res('don kd')
+        rej('don kd')
     }, 2000);
 })
 // console.log(waka)
 waka.then((res)=>{
     console.log('promise olevoir')
     console.log(res)
+}).catch((rej)=>{
+    
 })

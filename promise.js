eb6fc763393a7448d854eaa33849650370d0b1ef
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-17 10:29:38 +0200

    chaining then functions when returning promise

--- promise.js
+++ promise.js
@@ -1,13 +1,18 @@
-const waka = new Promise((res,rej)=>{
-    setTimeout(() => {
-        console.log('Waka Waka!');
-        rej('don kd')
-    }, 2000);
-})
+const waka = new Promise((res, rej) => {
+  setTimeout(() => {
+    console.log("Waka Waka!");
+    res("don kd");
+  }, 2000);
+});
 // console.log(waka)
-waka.then((res)=>{
-    console.log('promise olevoir')
-    console.log(res)
-}).catch((rej)=>{
-    
-})
+waka.then((res) => {
+    console.log("promise olevoir");
+    console.log(res);
+    return "impala";
+  })
+  .then((chr) => {
+    console.log(chr);
+  })
+  .catch((error) => {
+    console.log(error);
+  });

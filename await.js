2b0c675707281edffc96a0f41aa6e6a5ac752c4c
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-20 12:48:21 +0200

    creating login function

--- await.js
+++ await.js
@@ -2,7 +2,43 @@
 const fetchPokemon = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve({data: {name: 'pikachu', type: 'electric'}});
+            // resolve({data: {name: 'pikachu', type: 'electric'}});
+            reject(new Error('Failed to fetch Pokemon'));
         }, 2000);
     })
+}
+console.log('it\'s started')
+
+const usePoke = async () => {
+    try{
+    const data = await fetchPokemon()
+    console.log(data)
+    }catch(err){
+        console.error(err)
+    }
+}
+usePoke()
+console.log('program done!')
+
+const fetchUser = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({name: 'John Doe', age: 30, admin: true});
+        }, 3000);
+    })
+}
+const login = (user) => {
+if(user.admin === true){
+    console.log('welcome admin')
+}else{
+    console.log('unable to log in')
+}
+}
+
+const useUser = async () => {
+    try {
+        
+    } catch (error) {
+        console.error(error);
+    }
 }

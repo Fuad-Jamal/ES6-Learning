adfeff5e14a7b3321b222ee2ced157147672d21f
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-12 19:19:03 +0200

    getter functions

--- Mosh tutorials/mosh.js
+++ Mosh tutorials/mosh.js
@@ -1,18 +1,40 @@
 //factory function
-function createCircle (){
+function createCircle (radius){
 return{
     radius,
-   draw: function(){
+    draw: function(){
     console.log('draw');
    }
-}
+};
 }
 const circle = createCircle(1);
 
 //constructor function
 function Circle(radius){
+    // console.log('this', this);
     this.radius = radius
+    let defaultLocation = {x:0, y:0}
+    this.computeOptimumLocation = function(factor){}
+
     this.draw = function(){
-        console.log('draw');
+        this.computeOptimumLocation(0.1)
     }
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function(){
+            return defaultLocation
+        }
+    })
 }
+
+
+Circle.call({}, 1)
+Circle.apply({}, [1, 2, 3])
+
+const second = new Circle(1)
+
+for (let key in second){
+    console.log(key, second[key]);
+}
+const keys = Object.keys(second)
+console.log(Circle.defaultLocation)

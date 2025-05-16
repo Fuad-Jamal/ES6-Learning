7b84c531d9e2b7229ac9746b9d86c9c8af26a273
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-16 12:04:53 +0200

    creating a function that counts seconds of the parameter

---
+++ seconds.js
@@ -0,0 +1,28 @@
+let timer = (n){
+    n = Number(n)
+    if(n == 0){
+        return "now"
+    }
+const year = 31536000
+const day = 86400
+const hour = 3600
+const minute = 60
+const seconds = 1
+
+const sections = [
+    {name: 'year', seconds: 'value'},
+    {name: 'day', seconds: value},
+    {name: 'hour', seconds: value},
+    {name: 'minute', seconds: value},
+    {name: 'second', seconds: value}
+]
+
+const result = []
+
+for(const {name, second} of sections){
+    let counter = n/second
+    if(counter > 0){
+        n = n % second
+    }
+}
+}

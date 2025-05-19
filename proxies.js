da4109525a126a32e3eee9884e871f473ae10afa
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-19 19:21:53 +0200

    using the traps methods

--- proxies.js
+++ proxies.js
@@ -1,9 +1,13 @@
-let snake = {
-    name:'python',
-    weight: 20
-}
-let zoo = {
-    type: "anaconda"
-}
-let about = new Proxy(snake,zoo)
-console.log(about)
+// let snake = {
+//     name:'python',
+//     weight: 20
+// }
+// let zoo = {
+//     type: "anaconda"
+// }
+// let about = new Proxy(snake,zoo)
+// console.log(about)
+
+const gameSettings = {difficulty: 'easy'}
+const gameSettingsProxy = new Proxy(gameSettings, {})
+console.log(gameSettingsProxy.difficulty)

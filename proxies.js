068d27434dc68a0c4ad238d4399f783446cbf428
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-20 08:53:45 +0200

    getters and setters in proxies

--- proxies.js
+++ proxies.js
@@ -8,6 +8,24 @@
 // let about = new Proxy(snake,zoo)
 // console.log(about)
 
-const gameSettings = {difficulty: 'easy'}
-const gameSettingsProxy = new Proxy(gameSettings, {})
+const gameSettings = {}
+const gameSettingsProxy = new Proxy(gameSettings, {
+    get: (o, prop) =>{
+return prop in o? o[prop]: 'dudu'
+    }, 
+    set:(o, prop, value) =>{
+        if(prop === 'difficulty' && value !== ['easy', 'medium', 'hard'].includes(value.toLowerCase())){
+            throw new Error('Set difficulty please')
+        }
+
+        o[prop] = value
+    },
+    has: (o, prop) =>{
+        if(prop === 'difficulty'){
+            return false
+        }
+        retur prop in o
+    }
+})
+gameSettingsProxy.difficulty = 'esy'
 console.log(gameSettingsProxy.difficulty)

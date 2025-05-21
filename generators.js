b12ea563ae1f50ff2b8dbc3bd3fa4e227ab2b9e1
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-21 08:48:55 +0200

    yielding values to console

--- generators.js
+++ generators.js
@@ -1,15 +1,27 @@
+// function *generating () {
+//     let x = 7
+//     if( x => 0){
+//         console.log('wataguan') 
+//     }
+//     console.log(0)
+//     let yank = [4,2,2,4,5,2,5]
+//     for (let yan of yank){
+//         console.log(yan)
+//         yield;
+//     }
+// }
+// let xiq = generating()
+// xiq.next()
+// xiq.next()
+
+
 function *generating () {
-    let x = 7
-    if( x => 0){
-        console.log('wataguan') 
-    }
-    console.log(0)
-    let yank = [4,2,2,4,5,2,5]
-    for (let yan of yank){
-        console.log(yan)
-        yield;
-    }
+    yield 1
+    yield 2
+    yield 3
 }
-let xiq = generating()
-xiq.next()
-xiq.next()
+const generator = generating()
+console.log(generator)
+
+
+console.log(generator.next())

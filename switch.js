6e2b58df60fa1b87ffc61824e458db0c74a41fff
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-09 15:10:09 +0200

    creating object; ; ;

---
+++ switch.js
@@ -0,0 +1,6 @@
+const enemies = {
+chew: {atk: 10, def: 2, hp: 4, class: 3},
+puf: {atk: 1, def: 5, hp: 2, class: 8},
+heni: {atk: 40, def: 12, hp: 44, class: 33},
+eew: {atk: 20, def: 12, hp: 9, class: 9}
+}

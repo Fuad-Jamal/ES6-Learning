f43d8b9ee38e60b1eddfd92d57011baaebd0bcc0
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-10 10:58:04 +0200

    looping in the object alues

--- switch.js
+++ switch.js
@@ -4,3 +4,7 @@ puf: {atk: 1, def: 5, hp: 2, class: 8},
 heni: {atk: 40, def: 12, hp: 44, class: 33},
 eew: {atk: 20, def: 12, hp: 9, class: 9}
 }
+
+for(const ene of Object.values(enemies)){
+console.log(ene)
+}

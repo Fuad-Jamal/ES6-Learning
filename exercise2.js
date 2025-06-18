e867e892264bc76ab2491cfa7a024f461bac0c02
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-18 15:46:07 +0200

    calculate function run time

--- exercise2.js
+++ exercise2.js
@@ -1,4 +1,18 @@
 function getUser(name) {
     return `Welcome back, dear ${name}!`;
 }
-console.log(getUser('John')); // Welcome back, dear John!
+console.log(getUser('John'))
+
+
+function add(a, b) {
+    return a + b;
+}
+function track(add){
+    let begin = performance.now()
+    let result = add(2,3)
+    let end = performance.now()
+    let time = end - begin
+    console.log(`Function executed in ${time} milliseconds`)
+    return result
+}
+console.log(track(add))

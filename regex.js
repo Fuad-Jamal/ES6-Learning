840332be50cb4e07cb230eda148848d7f14fde98
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-19 15:07:50 +0200

    adding digits and finding them with regex

--- regex.js
+++ regex.js
@@ -1,5 +1,5 @@
-const sentence = 'there is a guy who is a good guy';
-const pattern = /w[^abc]o/;
+const sentence = 'there is a guy w4o is a good guy';
+const pattern = /w[\d]o/;
 const test = pattern.test(sentence);
 console.log(test);
 const match = sentence.match(pattern)

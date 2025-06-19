aa2f489683c8f8ba0606ea9fd75f3134d5ac37c0
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-19 15:29:36 +0200

    checking for possible variations

--- regex.js
+++ regex.js
@@ -1,5 +1,5 @@
 const sentence = 'there is a guy w4o is a good guy';
-const pattern = /^There/i;
+const pattern = /a/ig;
 const test = pattern.test(sentence);
 console.log(test);
 const match = sentence.match(pattern)

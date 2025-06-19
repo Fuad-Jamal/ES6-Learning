286871ac3250818b8a623af05c6c4bd2c19685c2
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-19 09:47:27 +0200

    adding a discount price

--- exercise3.js
+++ exercise3.js
@@ -7,6 +7,7 @@ class product {
         console.log(`The product we have is ${this.name} which costs about ${this.price}`)
     }
     getDiscount(disc){
-        git a
+        this.price = this.price * disc/100
+        console.log(`The product ${this.name} is now available at a discounted price of ${this.price}`)
     }
 }

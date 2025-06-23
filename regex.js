ccbffeda04717b612c07fb5de5cbf64745604ba2
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-23 12:06:21 +0200

    testing with regex expressions exercise

--- regex.js
+++ regex.js
@@ -1,6 +1,12 @@
-const sentence = 'there is a guy w4o is a good guy';
-const pattern = /a/ig;
+const sentence = 'there is a guy w4o is a good guy..&';
+const pattern = /\.\.\&/;
 const test = pattern.test(sentence);
 console.log(test);
 const match = sentence.match(pattern)
 console.log(match);
+
+const quote = 'I am a "good" guy'
+const patter = /am/
+const tes = patter.test(quote)
+console.log(tes)
+console.log(quote.match(patter))

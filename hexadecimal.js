5605a401a9dc27ca82e08c372eb375285252b339
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-23 08:11:25 +0200

    rounding to a fixed number of digits

--- hexadecimal.js
+++ hexadecimal.js
@@ -1,6 +1,11 @@
-function color(){
-    let one = Math.floor(Math.random()*0xFFFFFF)
-    let hexade = one.toString(16)
-    return '#' + hexade
+// function color(){
+//     let one = Math.floor(Math.random()*0xFFFFFF)
+//     let hexade = one.toString(16)
+//     return '#' + hexade
+// }
+// console.log(color())
+
+function round(a,b){
+ return parseFloat(a.toFixed(b))
 }
-console.log(color())
+console.log(round(1.234567, 2))

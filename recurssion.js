a3598225976e25141f88cbdbeac48bdee159f67b
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-24 10:42:40 +0200

    returning and index of recursive function

---
+++ recurssion.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sum = (array, index) => {
+    if(index === arr.length - 1){
+        console.log(`base case reached at index: ${index}`)
+        return array[index]
+    }
+    console.log(`current index: ${index}, current value: ${array[index]}`)
+    return array[index] + sum(array, index + 1)
+}
+const result = sum(arr, 2)
+console.log(result)

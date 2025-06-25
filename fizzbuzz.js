c68bf528c2e6a82044be9eac0e0258f57b758d33
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-25 10:06:39 +0200

    setting up a palindrome function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -13,4 +13,17 @@ const fizz = (start, end) => {
     }
     fizz(start + 1, end)
 }
-fizz(1, 100)
+// fizz(1, 100)
+
+function palinDrome(str){
+    if(str === '' || str.length === 1 || str.length=== 0 || (str[0]===str[1])) return true;
+}
+
+// Test cases for palindrome strings
+console.log(palinDrome("racecar")) // true
+console.log(palinDrome("hello")) // false
+console.log(palinDrome("A man a plan a canal Panama")) // true
+console.log(palinDrome("No lemon, no melon")) // true
+console.log(palinDrome("Was it a car or a cat I saw?")) // true
+console.log(palinDrome("12321")) // true
+console.log(palinDrome("12345")) // false

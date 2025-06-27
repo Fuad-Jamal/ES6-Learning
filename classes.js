080f508909f228d686dd60240d96992a3c8d353a
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-27 11:50:56 +0200

    creating an animal class

---
+++ classes.js
@@ -0,0 +1,11 @@
+class Animal {
+    constructor(type, name){
+        this.name = name
+        this.type = type
+    }
+
+}
+const one = new Animal ('Dog', 'Buddy')
+const two = new Animal ('Cat', 'Whiskers')
+console.log(one)
+console.log(two)

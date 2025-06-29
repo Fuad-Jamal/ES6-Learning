9978176c86b5c849b4c2b260146f96cd511d7e32
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-29 14:40:56 +0200

    creating database with classes

--- classes.js
+++ classes.js
@@ -1,37 +1,13 @@
-class Enemy{
-    generateEnemy(name){
-        class Flying{
-            constructor(flyName){
-                this.flyName = flyName
-            }
-            fly(){
-                console.log(`${this.flyName} is flying!`)
-            }
-        }
-        return new Flying(name)
+class Database{
+    token = 'abc242'
+    constructor() {}
+    
+    saveData(data) {
+        this.initDBConnection()
+        this.authorize()
     }
+    
+    initDBConnection(){}
+    authorize(){}
+    updateQuery(){}
 }
-
-Enemy.generateEnemy = function(name){
-    class Swimming{
-        name = name
-
-        swim(){
-            console.log(`${this.name} is swimming!`)
-        }
-    }
-    return new Swimming(name)
-}
-
-const facotry = new Enemy()
-const flying = facotry.generateEnemy('Panther')
-flying.fly()
-
-const swimming = Enemy.generateEnemy('Shark')
-swimming.swim()
-
-console.log(Object.getOwnPropertyNames(Enemy))
-console.log(Object.getOwnPropertyNames(facotry))
-console.log(Object.getOwnPropertyNames(facotry.__proto__))
-console.log(Object.getOwnPropertyNames(flying.__proto__))
-console.log(Object.getOwnPropertyNames(swimming.__proto__))

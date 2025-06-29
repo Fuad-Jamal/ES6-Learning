bb690a9d5092681cd77c8fd321ed08a42302ee94
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-29 11:25:00 +0200

    creating an enemy factory

--- classes.js
+++ classes.js
@@ -1,23 +1,12 @@
-class Bookstore {
-    constructor(books){
-        this.books = books
-    }
-    listBooks(){
-    for(const book of this.books){
-        console.log(`Book: ${book.name}, Author: ${book.author}`)
-    }
-}
-}
-class Book{
-    constructor(name, author){
-        this.name = name
-        this.author = author
-    }
-    display(){
-        console.log(`Title: ${this.name}, Author: ${this.author}`)
+class Enemy{
+    generateEnemy(name){
+        class Flying{
+            constructor(flyName){
+                this.flyName = flyName
+            }
+        }
     }
 }
-const halidon = new Book('The Halidon', 'Robert Ludlum')
-const hp = new Book('Harry Potter', 'J.K. Rowling')
-const bookstore = new Bookstore([halidon, hp])
-bookstore.listBooks()
+
+const facotry = new Enemy()
+const flying = facotry.generateEnemy('Panther')

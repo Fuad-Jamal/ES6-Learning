2299884a222c3cb2b0a93d9872af26f6deb2bfcf
Fuad Jamal <jamalfuad34@gmail.com>
2025-06-29 11:40:03 +0200

    creating instances with classes that return other classes

--- classes.js
+++ classes.js
@@ -4,9 +4,28 @@ class Enemy{
             constructor(flyName){
                 this.flyName = flyName
             }
+            fly(){
+                console.log(`${this.flyName} is flying!`)
+            }
+        }
+        return new Flying(name)
+    }
+}
+
+Enemy.generateEnemy = function(name){
+    class Swimming{
+        name = name
+
+        swim(){
+            console.log(`${this.name} is swimming!`)
         }
     }
+    return new Swimming(name)
 }
 
 const facotry = new Enemy()
 const flying = facotry.generateEnemy('Panther')
+flying.fly()
+
+const swimming = Enemy.generateEnemy('Shark')
+swimming.swim()

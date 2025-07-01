2037c9202f28a6aa0cf5e3dbf32a4c39b98546f5
Fuad Jamal <jamalfuad34@gmail.com>
2025-07-01 10:41:13 +0200

    context of this without prototype

--- this.js
+++ this.js
@@ -1,8 +1,10 @@
-thro= {
+thrro= {
     name: 'Thro',
-    age: 25
-}
-Object.prototype.greet = function() {
+    age: 25,
+    greet: function() {
+    console.log(this)
     console.log(`Hello, I am ${this.name}`)
 }
-thro.greet()
+}
+// Object.prototype.
+thrro.greet()

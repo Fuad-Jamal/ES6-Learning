c59028d43bb285f812cd333b174f1473c5afe355
Fuad Jamal <jamalfuad34@gmail.com>
2025-07-01 13:09:48 +0200

    creating pet objects and how they define the this context

--- this.js
+++ this.js
@@ -13,3 +13,14 @@ arrow: () => {
 const x = thrro.greet.bind({ name: 'John' })
 console.log(x)
 x('ksj', 'kjsd')
+
+
+const pet = {name: 'Dog', age: 5, type: 'Mammal'}
+
+Object.prototype.sayHello = function() {
+    console.log(`Hello, I am a ${this.type} named ${this.name} and I am ${this.age} years old.`)
+}
+pet.sayHello()
+
+const pet2 = {name: 'Cat', age: 3, type: 'Mammal'}
+pet2.sayHello()

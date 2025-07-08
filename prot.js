81be9d565acf39822f6b019d38df4a5ad06e2c92
Fuad Jamal <jamalfuad34@gmail.com>
2025-07-08 11:48:56 +0200

    adding a prototype for the object

--- prot.js
+++ prot.js
@@ -1,6 +1,13 @@
+Object.prototype.speak = ()=>{
+    console.log(`I am a ${this.type} with ${this.legs} legs.`);
+}
+
 const animal = {
     type: 'monkey',
     legs: 2,
 }
 console.log(animal.toString())
 console.log(animal.__proto__.__proto__)
+console.log(Object.getOwnPropertyNames(animal.legs.__proto__))
+
+animal.speak()

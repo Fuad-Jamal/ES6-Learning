5f88f31657ac47559af76eb16955cdecb4b27bed
Fuad Jamal <jamalfuad34@gmail.com>
2025-08-10 19:57:40 +0200

    timinh the fastes promise

--- promise.js
+++ promise.js
@@ -1,43 +1,43 @@
-// const waka = new Promise((res, rej) => {
-//   setTimeout(() => {
-//     console.log("Waka Waka!");
-//     rej("don kd");
-//   }, 2000);
-// });
-// // console.log(waka)
-// waka.then((res) => {
-//     console.log("promise olevoir");
-//     console.log(res);
-//     console.log(waka)
-//     return "impala";
-//   })
-//   .then((chr) => {
-//     console.log(chr);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//     console.log(waka)
-//   });
+const waka = new Promise((res, rej) => {
+  setTimeout(() => {
+    console.log("Waka Waka!");
+    rej("don kd");
+  }, 2000);
+});
+// console.log(waka)
+waka.then((res) => {
+    console.log("promise olevoir");
+    console.log(res);
+    console.log(waka)
+    return "impala";
+  })
+  .then((chr) => {
+    console.log(chr);
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log(waka)
+  });
 
-// console.log("program start")
-// let saka = new Promise((resolve, reject) => {
-//     setTimeout(()=>{
-// resolve('promise resolved')
-//     },3000)
-// }).then((res)=>{
-//     console.log(res)
-//     return new Promise ((resolve, reject) => {
-//         setTimeout(()=>{
-//             resolve('second promise resolved')
-//         },3000)
-//     })
-// }).then((res)=>{
-//     console.log(res)
-// })
-// .catch((err)=>{
-//     console.log(err)
-// })
-// console.log(saka)
+console.log("program start")
+let saka = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+resolve('promise resolved')
+    },3000)
+}).then((res)=>{
+    console.log(res)
+    return new Promise ((resolve, reject) => {
+        setTimeout(()=>{
+            resolve('second promise resolved')
+        },3000)
+    })
+}).then((res)=>{
+    console.log(res)
+})
+.catch((err)=>{
+    console.log(err)
+})
+console.log(saka)
 
 
 const one = new Promise ((resolve, reject) => {

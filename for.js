6f7e5ceab6aabc05424624dc92b71ba65043f97d
Fuad Jamal <jamalfuad34@gmail.com>
2025-08-21 10:58:37 +0200

    using for in loop exercise

--- for.js
+++ for.js
@@ -7,4 +7,11 @@ for(let str = 'baaa', i = 0, j=10; str != 'maaa'; i++, j++){
     }
     console.log(str)
 }
-
+const person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+}
+for(let key in person){
+    console.log(person[key])
+}

4a253eebe873bd001cb0dbd035819cd4ac0523e1
Fuad Jamal <jamalfuad34@gmail.com>
2025-08-25 20:06:31 +0200

    looping through rgb colors

--- hexadecimal.js
+++ hexadecimal.js
@@ -1,9 +1,9 @@
-// function color(){
-//     let one = Math.floor(Math.random()*0xFFFFFF)
-//     let hexade = one.toString(16)
-//     return '#' + hexade
-// }
-// console.log(color())
+function color(){
+    let one = Math.floor(Math.random()*0xFFFFFF)
+    let hexade = one.toString(16)
+    return '#' + hexade
+}
+console.log(color())
 
 function round(a,b){
  return parseFloat(a.toFixed(b))
